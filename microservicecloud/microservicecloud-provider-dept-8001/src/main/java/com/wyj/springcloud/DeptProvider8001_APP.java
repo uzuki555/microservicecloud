2edebe955140639a33087846a57ce6a180983b44
wyj <aaa314568649@qq.com>
2019-08-15 18:44:27 +0800

    20190815

--- microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/wyj/springcloud/DeptProvider8001_APP.java
+++ microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/wyj/springcloud/DeptProvider8001_APP.java
@@ -2,14 +2,12 @@ package com.wyj.springcloud;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
-
+@EnableEurekaClient
 @Configuration
 @MapperScan("com.wyj.springcloud.dao")
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class DeptProvider8001_APP {
     public static void main(String[] args) {
