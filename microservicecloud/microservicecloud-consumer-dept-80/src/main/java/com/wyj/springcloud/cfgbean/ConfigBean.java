6acec054ad505353702b5cc0d228fc2dce5213e9
wyj <aaa314568649@qq.com>
2019-08-19 18:38:18 +0800

    20190819 18:38

--- microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/wyj/springcloud/cfgbean/ConfigBean.java
+++ microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/wyj/springcloud/cfgbean/ConfigBean.java
@@ -1,5 +1,8 @@
 package com.wyj.springcloud.cfgbean;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,8 +10,13 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ConfigBean {
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate(){
         return  new RestTemplate();
     }
 
+    @Bean
+    public IRule myRule(){
+        return new RandomRule();
+    }
 }
---
+++ microservicecloud/microservicecloud-consumer-dept-feign/src/main/java/com/wyj/springcloud/App.java
@@ -0,0 +1,12 @@
+package com.wyj.springcloud;
+
+/**
+ * Hello world! *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
